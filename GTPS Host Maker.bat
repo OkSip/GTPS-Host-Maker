fc6f449abb73a47c8ba9c00e8b3a942fe02a89ed
OkSipXD <68255754+OkSip@users.noreply.github.com>
2020-10-16 10:07:19 +0700

    Add files via upload

---
+++ GTPS Host Maker.bat
@@ -0,0 +1,13 @@
+@echo off
+:main
+title Coded By OkSipXD#3069
+color 9
+echo GTPS Host Maker By OkSipXD#3069
+echo =================================
+set /p ip=Ip Adress : 
+:host
+del host.txt
+echo %ip% growtopia1.com>>host.txt
+echo %ip% growtopia2.com>>host.txt
+echo Your Host Successfully Created
+timeout 2 > nul
